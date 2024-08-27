7d6cb2d1539ad75aee2f6775d94347207c25c598
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-08-27 23:10:26 +0900

    feat: 단어의 개수 BOJ_1152.java

---
+++ kimchanho/2week/BOJ_1152.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_1152 {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+    int length = stringTokenizer.countTokens();
+    bw.write(String.valueOf(length));
+    bw.flush();
+  }
+}
