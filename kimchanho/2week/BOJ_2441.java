f85907ec1dcbd7775e5bc805aa57e7e939f53192
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-08-27 23:21:42 +0900

    feat: 별 찍기-4 BOJ_2441.java

---
+++ kimchanho/2week/BOJ_2441.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_2441 {
+
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(reader.readLine());
+    for (int i = n; i > 0; i--) {
+      writer.write(" ".repeat(n - i));
+      writer.write("*".repeat(i));
+      writer.newLine();
+    }
+    writer.flush();
+  }
+}
