acfc07faa154ac3a4182b5252dcc21739ac60d9c
Park107 <sanghee.park107@gmail.com>
2024-08-27 23:53:15 +0900

    단어 공부 BOJ_1157.py

---
+++ sanghee/1week/BOJ_1157.py
@@ -0,0 +1,15 @@
+from collections import Counter
+word_original = input()
+
+def get_answer(word_original):
+  word = word_original.upper()
+  count_dict = Counter(word)
+  max_count = max(count_dict.values())
+  max_letters_list = [char for char, count in count_dict.items() if count == max_count]
+
+  if len(max_letters_list) == 1:
+    print(max_letters_list[0])
+  else:
+    print("?")
+
+get_answer(word_original)
