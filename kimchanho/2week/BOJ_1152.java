3cbb739c3d10aaf389b90fd7d0af47f89843fbed
Kim.Chanho <kiss.yagni.dry@gmail.com>
2024-08-29 10:23:33 +0900

    refactor: 단어의 개수 BOJ_1152의 풀이방식 2개 추가
    # 설명
    1. StringTokenizer를 사용
    2. String.split를 사용
    3. 정규식 사용

--- kimchanho/2week/BOJ_1152.java
+++ kimchanho/2week/BOJ_1152.java
@@ -1,18 +1,45 @@
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BOJ_1152 {
 
-  public static void main(String[] args) throws IOException {
+  // StringTokenizer 사용
+  public static void solution1(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
     int length = stringTokenizer.countTokens();
     bw.write(String.valueOf(length));
+    bw.flush();
+  }
+
+  // split 사용
+  public static void solution2(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String[] words = br.readLine().trim().split(" ");
+    bw.write(String.valueOf(words.length));
+    bw.flush();
+  }
+
+  // 정규식 사용
+  public static void solution3(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    String sentence = " " + br.readLine();
+    Pattern pattern = Pattern.compile(" [a-zA-Z]");
+    Matcher matcher = pattern.matcher(sentence);
+    long count = matcher.results().count();
+    bw.write(String.valueOf(count));
     bw.flush();
   }
 }
