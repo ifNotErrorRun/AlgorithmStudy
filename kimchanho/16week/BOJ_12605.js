8f8113f08500914c242a7674efb9c267887dc378
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-12-21 14:23:01 +0900

    feat: 단어순서 뒤집기 BOJ_12605.js

---
+++ kimchanho/16week/BOJ_12605.js
@@ -0,0 +1,17 @@
+const solution = (input) => {
+  const N = Number(input[0]);
+  for (let i = 1; i <= N; i++) {
+    const words = input[i].split(" ");
+    let result = "";
+    while (words.length) {
+      result += words.pop() + " ";
+    }
+    console.log(`Case #${i}: ${result}`);
+  }
+};
+
+const inputArr = [];
+require("readline")
+  .createInterface({ input: process.stdin, output: process.stdout })
+  .on("line", (line) => inputArr.push(line))
+  .on("close", () => solution(inputArr));
