1b14558df8f7a4dd66224c31a20d99122534cd6a
Kim Chanho <kiss.yagni.dry@gmail.com>
2024-12-21 14:45:54 +0900

    feat: 괄호 BOJ_9012.js

---
+++ kimchanho/16week/BOJ_9012.js
@@ -0,0 +1,31 @@
+const solution = (inputArr) => {
+  const N = Number(inputArr[0]);
+  for (let i = 1; i <= N; i++) {
+    const input = inputArr[i].split("");
+    isVPS(input);
+  }
+};
+
+const isVPS = (input) => {
+  const stack = [];
+
+  while (input.length) {
+    const bracket = input.shift();
+    if (bracket === "(") {
+      stack.push(bracket);
+    } else {
+      if (stack.length === 0) {
+        console.log("NO");
+        return;
+      }
+      stack.pop();
+    }
+  }
+  console.log(stack.length === 0 ? "YES" : "NO");
+};
+
+const inputArr = [];
+require("readline")
+  .createInterface({ input: process.stdin, output: process.stdout })
+  .on("line", (line) => inputArr.push(line))
+  .on("close", () => solution(inputArr));
