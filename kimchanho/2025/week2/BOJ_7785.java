ccaa127d4fea85a7ed001b12995c70d31105c5af
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-01-10 01:54:21 +0900

    comment: 회사에 있는 사람 BOJ_7785

--- kimchanho/2025/week2/BOJ_7785.java
+++ kimchanho/2025/week2/BOJ_7785.java
@@ -5,6 +5,12 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+/**
+ * @title 회사에 있는 사람 (7785)
+ * @description 회사에 남아 있는 사람 수를 세는 문제
+ * @author Chanho Kim
+ * @see https://www.acmicpc.net/problem/7785
+ */
 public class BOJ_7785 {
 
   public static void main(String[] args) throws Exception {
