f296416b0c32ec8eb09801ad27a4ab363fcbd926
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-01-23 22:06:58 +0900

    comment: 주석 추가

--- kimchanho/2025/week3/BOJ_25497.java
+++ kimchanho/2025/week3/BOJ_25497.java
@@ -1,36 +1,45 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
+/**
+ * @title 기술 연계마스터 임스 (25497)
+ * @description 기술을 사용하는 순서에 따라서 몇 번 기술을 사용할 수 있는지 출력하는 문제
+ * @author Chanho Kim
+ * @see https://www.acmicpc.net/problem/25497
+ */
 public class BOJ_25497 {
 
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     br.readLine();
     char[] commands = br.readLine().toCharArray();
-    Map<Character, Character> preSkill = Map.of('R', 'L', 'K', 'S');
-    Map<Character, Integer> preSkillCount = new HashMap<>();
-    preSkillCount.put('L', 0);
-    preSkillCount.put('S', 0);
 
-    int count = 0;
+    // 기술 연계맵과 기술 연계 큐
+    Map<Character, Character> comboMap = Map.of('R', 'L', 'K', 'S');
+    Map<Character, Queue<Character>> comboQueue = Map.of('L', new LinkedList<>(), 'S', new LinkedList<>());
+
+    int skillCount = 0;
     for (char command : commands) {
-      if (command == 'L' || command == 'S') {
-        preSkillCount.put(command, preSkillCount.get(command) + 1);
-      } else if (command == 'R' || command == 'K') {
-        if (preSkillCount.get(preSkill.get(command)) > 0) {
-          preSkillCount.put(preSkill.get(command), preSkillCount.get(preSkill.get(command)) - 1);
-          count++;
-        } else {
-          break;
-        }
+      // 입력키가 연계키인 경우
+      if (comboMap.containsValue(command)) {
+        comboQueue.get(command).offer(command);
       } else {
-        count++;
+        // 입력키가 기술키인 경우
+        if (comboMap.containsKey(command)) {
+          // 기술키를 가지고, 해당 큐에서 연계키 꺼내기를 시도, 없으면 null이 반횐되므로 종료
+          Character cur = comboQueue.get(comboMap.get(command)).poll();
+          //
+          if (cur == null) break;
+        }
+        skillCount++;
       }
     }
 
-    System.out.println(count);
+    System.out.println(skillCount);
   }
 
 }
--- kimchanho/2025/week3/BOJ_32979.java
+++ kimchanho/2025/week3/BOJ_32979.java
@@ -1,14 +1,21 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.stream.Collectors;
 
+/**
+ * @title 아파트 (32979)
+ * @description 손이 겹쳐진 상태에서 손을 빼는 순서에 따라서 당첨된 사람 번호를 출력하는 문제
+ * @author Chanho Kim
+ * @see https://www.acmicpc.net/problem/32979
+ */
 public class BOJ_32979 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    br.readLine();  // person: ignore
-    br.readLine(); // gameRound: ignore
+    br.readLine();  // person: 사용하지 않는 값 무시
+    br.readLine(); // gameRound: 사용하지 않는 값 무시
     Queue<Integer> hands = Arrays.stream(br.readLine().split(" "))
         .map(Integer::parseInt)
         .collect(Collectors.toCollection(LinkedList::new));
@@ -17,10 +24,13 @@ public class BOJ_32979 {
         .toArray();
 
     StringBuilder sb = new StringBuilder();
+    // 각 라운드별로 손을 빼는 순서에 따라서 당첨된 사람 번호를 출력
     for (int target : roundTarget) {
       for (int i = 0; i < target - 1; i++) {
+        // 손을 빼고 바로 큐에 넣기
         hands.offer(hands.poll());
       }
+      // 현재 가장 아래에 있는 손의 번호를 저장, 손을 빼지 않고 다음게임 진행
       sb.append(hands.peek()).append(" ");
     }
     System.out.println(sb);
