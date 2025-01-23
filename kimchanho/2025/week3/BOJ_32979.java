e91ab7c9a6de949d6957c02e823186cfd70500ec
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-01-23 23:46:54 +0900

    refactor: 아파트 BOJ_32979 - 인덱싱 계산방법으로 성능 개선

--- kimchanho/2025/week3/BOJ_32979.java
+++ kimchanho/2025/week3/BOJ_32979.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.stream.Collectors;
 
 /**
  * @title 아파트 (32979)
@@ -14,24 +11,19 @@ import java.util.stream.Collectors;
 public class BOJ_32979 {
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    br.readLine();  // person: 사용하지 않는 값 무시
-    br.readLine(); // gameRound: 사용하지 않는 값 무시
-    Queue<Integer> hands = Arrays.stream(br.readLine().split(" "))
-        .map(Integer::parseInt)
-        .collect(Collectors.toCollection(LinkedList::new));
+    br.readLine();  // person   : ignore
+    br.readLine();  // gameRound: ignore
+    String[] hands = br.readLine().split(" ");
     int[] roundTarget = Arrays.stream(br.readLine().split(" "))
         .mapToInt(Integer::parseInt)
         .toArray();
 
+    int index = 0;
     StringBuilder sb = new StringBuilder();
     // 각 라운드별로 손을 빼는 순서에 따라서 당첨된 사람 번호를 출력
     for (int target : roundTarget) {
-      for (int i = 0; i < target - 1; i++) {
-        // 손을 빼고 바로 큐에 넣기
-        hands.offer(hands.poll());
-      }
-      // 현재 가장 아래에 있는 손의 번호를 저장, 손을 빼지 않고 다음게임 진행
-      sb.append(hands.peek()).append(" ");
+      index = (index + target - 1) % hands.length;
+      sb.append(hands[index]).append(" ");
     }
     System.out.println(sb);
   }
