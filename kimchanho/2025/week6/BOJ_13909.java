f99a73bfea6c40b40f0057e0f087e8ddcbffeaf5
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-02-13 20:25:48 +0900

    feat: 창문 닫기 BOJ_13909.java

---
+++ kimchanho/2025/week6/BOJ_13909.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_13909 {
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    System.out.println((int) Math.sqrt(n));
+  }
+}
